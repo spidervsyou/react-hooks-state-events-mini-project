5bcb205d48439ee09d3385662405e616b7b35f2c
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-05-09 20:59:17 +0300

    Update CategoryFilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function CategoryFilter() {
+const CategoryFilter = ({ categories }) => {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+    <div>
+      <h2>Categories</h2>
+      {categories.map(category => (
+        <button
+          key={category}
+          className={category === selectedCategory ? 'selected' : ''}
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
-}
+};
 
 export default CategoryFilter;
