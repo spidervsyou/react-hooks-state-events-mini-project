b622a482850780729fb55a6d8d32810be045404e
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-05-09 20:57:21 +0300

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
+import Task from './Task';
 
-function TaskList() {
+const TaskList = ({ tasks, onDelete }) => {
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
+    <div>
+      <h2>Tasks</h2>
+      {tasks.map(task => (
+        <Task key={task.id} task={task} onDelete={onDelete} />
+      ))}
     </div>
   );
-}
+};
 
 export default TaskList;
